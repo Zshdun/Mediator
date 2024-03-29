bf3f443857401efbf633405273e7b2720528152b
Danya Samoylov <130479785+Zshdun@users.noreply.github.com>
2024-03-29 08:04:04 +0700

    Add files via upload

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.User2log = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.User1log = new System.Windows.Forms.TextBox();
+            this.User3log = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(238, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(480, 163);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // User2log
+            // 
+            this.User2log.Location = new System.Drawing.Point(517, 314);
+            this.User2log.Multiline = true;
+            this.User2log.Name = "User2log";
+            this.User2log.Size = new System.Drawing.Size(100, 100);
+            this.User2log.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(237, 314);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // User1log
+            // 
+            this.User1log.Location = new System.Drawing.Point(350, 175);
+            this.User1log.Multiline = true;
+            this.User1log.Name = "User1log";
+            this.User1log.Size = new System.Drawing.Size(100, 100);
+            this.User1log.TabIndex = 4;
+            // 
+            // User3log
+            // 
+            this.User3log.Location = new System.Drawing.Point(610, 187);
+            this.User3log.Multiline = true;
+            this.User3log.Name = "User3log";
+            this.User3log.Size = new System.Drawing.Size(100, 100);
+            this.User3log.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(517, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(341, 79);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.User3log);
+            this.Controls.Add(this.User1log);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.User2log);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox User2log;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox User1log;
+        private System.Windows.Forms.TextBox User3log;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static WindowsFormsApp6.Form1;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChatMediator mediator = new ChatMediator();
+
+            ChatUser user1 = new ChatUser(mediator, "User 1");
+            ChatUser user2 = new ChatUser(mediator, "User 2");
+            ChatUser user3 = new ChatUser(mediator, "User 3");
+
+            mediator.RegisterUser(user1);
+            mediator.RegisterUser(user2);
+            mediator.RegisterUser(user3);
+            string msg1 = textBox1.Text;
+            user1.SendMessage(msg1);
+            string msg2 = textBox2.Text;
+            user2.SendMessage(msg2);
+            string msg3 = textBox3.Text;
+            user3.SendMessage(msg3);
+
+            List<string> user1ReceivedMessages = user1.GetReceivedMessages();
+            foreach (var message in user1ReceivedMessages)
+            {
+                User1log.Text += message;
+                User1log.Text += "\r\n";
+            }
+            List<string> user2ReceivedMessages = user2.GetReceivedMessages();
+            foreach (var message in user2ReceivedMessages)
+            {
+                User2log.Text += message;
+                User2log.Text += "\r\n";
+            }
+            List<string> user3ReceivedMessages = user3.GetReceivedMessages();
+            foreach (var message in user3ReceivedMessages)
+            {
+                User3log.Text += message;
+                User3log.Text += "\r\n";
+            }
+
+        }
+
+        public class ChatMediator
+        {
+            private readonly List<IUser> _users;
+
+            public ChatMediator()
+            {
+                _users= new List<IUser>();
+            }
+
+            public void RegisterUser(IUser user)
+            {
+                _users.Add(user);
+            }
+            public void SendMessage(string senderName, string message)
+            {
+                foreach(var user in _users)
+                {
+
+                    if (!user.Name.Equals(senderName))
+                    {
+                        user.RecieveMessage(senderName, message);
+
+                    }
+                }
+                
+
+            }
+        
+        }
+        public interface IUser
+        {
+             string Name { get; }
+
+            void SendMessage(string message);
+
+            void RecieveMessage(string senderName, string message);
+        }
+        public class ChatUser : IUser
+        {
+            private readonly ChatMediator _mediator;
+            public List<string> receivedMessages;
+
+            public ChatUser(ChatMediator mediator, string name)
+            {
+                _mediator = mediator;
+                Name = name;
+                this.receivedMessages = new List<string>();
+            }
+            public string Name { get; }
+
+            public void SendMessage(string message)
+            {
+                //MessageBox.Show($"{Name}: Sending Message: {message}");
+                MessageBox.Show("Message sent!");
+                _mediator.SendMessage(senderName: Name, message: message);
+
+            }
+            public void RecieveMessage(string senderName, string message)
+            {
+                //MessageBox.Show($"{Name}: Recieved message : '{message}' from {senderName}");
+                receivedMessages.Add(message);
+
+            }
+            public List<string> GetReceivedMessages()
+            {
+                return receivedMessages;
+            }
+
+
+        }
+
+    }
+}
